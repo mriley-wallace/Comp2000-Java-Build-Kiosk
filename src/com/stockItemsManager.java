0ea0d969acd043a7374809f50d13492822a7cc62
mriley-wallace <mriley-wallace@users.noreply.github.com>
2021-01-09 17:47:07 +0000

    Commit 1.2c - Fixed array out of bounds exception
    So after a bit of debugging, java really doesnt like you pressing enter on a text file as the scanner reads it as a new line with no data and thinks everything in the array is just empty, which as floats and integers are concerned, considers the empty as "" which is a string.

--- src/com/stockItemsManager.java
+++ src/com/stockItemsManager.java
@@ -2,6 +2,8 @@ package com;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -14,7 +16,7 @@ public class stockItemsManager {
         return stock;
     }
 
-    private final ArrayList<stockItems> stock = new ArrayList<stockItems>();
+    private ArrayList<stockItems> stock = new ArrayList<stockItems>();
 
     public void stockLoad(){
 
@@ -28,13 +30,15 @@ public class stockItemsManager {
                 String[] stockData = dataRow.split(separator);
 
                 stockItems newStock = new stockItems();
+
                 newStock.setName(stockData[0]);
 
-                float priceToFloat = Float.parseFloat(stockData[1]);
-                newStock.setPrice(priceToFloat);
 
-                int amountToInt = Integer.parseInt(stockData[2]);
+                int amountToInt = Integer.parseInt(stockData[1]);
                 newStock.setAmount(amountToInt);
+
+                float priceToFloat = Float.parseFloat(stockData[2]);
+                newStock.setPrice(priceToFloat);
 
                 double barcodeToDouble = Double.parseDouble(stockData[3]);
                 newStock.setBarcode(barcodeToDouble);
@@ -48,9 +52,34 @@ public class stockItemsManager {
             }
             scanner.close();
 
-            System.out.println("Admin file successfully loaded");
+            System.out.println("Stock file successfully loaded");
         }
         catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    public void itemSave(){
+        try{
+            FileWriter writer = new FileWriter(filePath);
+
+            for(int index = 0; index < stock.size(); index++){
+                String dataRow = "";
+                if(index > 0){
+                    dataRow += "\n";
+                }
+                dataRow += stock.get(index).getName();
+                float priceToFloat = stock.get(index).getPrice();
+                int amountToInt = stock.get(index).getAmount();
+                double barcodeToDouble = stock.get(index).getBarcode();
+                int pluToInt = stock.get(index).getPlu();
+                dataRow += "|" + priceToFloat + "|" + amountToInt + "|" + barcodeToDouble + "|" + pluToInt;
+
+                writer.write(dataRow);
+            }
+            writer.close();
+        } catch (IOException e){
             e.printStackTrace();
         }
     }
